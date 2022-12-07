99d7f7e4076eff734b4414919692e8d8b465bb26
Stephan Pitois <spitois@gmail.com>
2022-12-06 20:54:19 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import App from './App.svelte';
 
+var target = document.body;
+if (typeof GLOBAL_MFE_PARENT_ID_MFE_1 !== 'undefined') {
+	target = document.getElementById('GLOBAL_MFE_PARENT_ID_MFE_1);
+}
+
 const app = new App({
-	target: GLOBAL_MFE__MFE_1 || document.body,
+	target: target,
 	props: {
 		name: 'world'
 	}
