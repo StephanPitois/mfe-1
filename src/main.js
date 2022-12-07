8accbe84fe6083a07765941c7f27c1093e89d38e
Stephan Pitois <spitois@gmail.com>
2022-12-06 20:54:35 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import App from './App.svelte';
 
 var target = document.body;
 if (typeof GLOBAL_MFE_PARENT_ID_MFE_1 !== 'undefined') {
-	target = document.getElementById('GLOBAL_MFE_PARENT_ID_MFE_1);
+	target = document.getElementById(GLOBAL_MFE_PARENT_ID_MFE_1);
 }
 
 const app = new App({
