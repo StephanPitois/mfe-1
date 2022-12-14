f1508cd901b7aa8ba541ab42c5d7e90e5cfae31c
Stephan Pitois <spitois@gmail.com>
2022-12-13 22:53:50 -0500

    Clarifications + UI tweaks

--- src/Totoro.svelte
+++ src/Totoro.svelte
@@ -4,16 +4,15 @@
 	import "./lib/SomeComponent.svelte";
 	import { count } from "./lib/stores.js";
 
-	let defaultName1 =
-		"Satsuki. I update Totoro's STORE and trigger a GLOBAL EVENT";
+	let defaultName1 = "Satsuki. I update Totoro's store $count and trigger a TOTORO_WAS_CLICKED event";
 	let name1 = defaultName1;
 
 	let defaultName2 =
-		"Mei. I update Totoro's STORE BUT I do NOT trigger a global event";
+		"Mei. I update Totoro's store $count without triggering any global events";
 	let name2 = defaultName2;
 
 	function handleClick1() {
-		name1 = `${defaultName1} and the time is ${new Date().toLocaleTimeString()}`;
+		name1 = defaultName1;
 		$count++;
 
 		// Use this to communicate with other micro-frontends:
@@ -25,49 +24,58 @@
 	}
 
 	function handleClick2() {
-		name2 = `${defaultName2} and the time is ${new Date().toLocaleTimeString()}`;
+		name2 = defaultName2;
 		$count++;
 	}
 </script>
 
-<main part="mfeMain">
-	<h1 part="mfeMainH1">MFE 1: Totoro (トトロ, Totoro)</h1>
-	<img
-		part="mfeMainImg"
-		src="/img/totoro.jpeg"
-		alt="The main characters of My Neighbor Totoro."
-	/>
-	<p class="info">
-		Event-based MFE communication only works when MFEs are 
-		on the same page. That's not an issue because the goal
-		of these events if to ensure that the entire page is in
-		an up-to-date, consistent state. Whenever the user navigates
-		to another page, the MFEs on that page are responsible
-		for updating their own state. If that process is too
-		much of a burden, we will look into ways for the app shell
-		to help pass the global state when navigating to another page,
-		either in-memory of via session storage.
-	</p>
-	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<fl-somecomponent name={name1} on:click={handleClick1} />
-	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<fl-somecomponent name={name2} on:click={handleClick2}>
-		<p part="mfeMainP">
-			This is some child content that will overwrite the default slot
-			content. With Svelte, slots are loaded lazily, but when compiled to
-			custom elements (web components), they are rendered eagerly. This is
-			just something to know, not necessarily an issue. It does not remove
-			any of the dynamic behavior within a slot. For example, whenever
-			<code>$count</code>
-			is updated dynamically, this slot is re-rendered accordingly. Proof:
-			<code>$count={$count}</code>.
-		</p>
-	</fl-somecomponent>
-</main>
+<div part="mfeCard">
+	<div part="mfeCardTitle">Micro-Frontend</div>
+	<div part="mfeCardBody">
+		<main part="mfeMain">
+			<h1 part="mfeMainH1">MFE 1: Totoro (トトロ, Totoro)</h1>
+			<div class="flex">
+				<img
+					part="mfeMainImg"
+					src="/img/totoro.jpeg"
+					alt="The main characters of My Neighbor Totoro."
+				/>
+				<div part="mfeCard">
+					<div part="mfeCardTitle">Stores owned by Totoro</div>
+					<div part="mfeCardBody" class="mfeCardBody">
+						<code>$count = {$count}</code>
+					</div>
+				</div>
+			</div>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<fl-somecomponent name={name1} on:click={handleClick1} />
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<fl-somecomponent name={name2} on:click={handleClick2}>
+				<p part="mfeMainP">
+					This is some child content that will overwrite the default
+					slot content. With Svelte, slots are loaded lazily, but when
+					compiled to custom elements (web components), they are
+					rendered eagerly. This is just something to know, not
+					necessarily an issue. It does not remove any of the dynamic
+					behavior within a slot. For example, whenever
+					<code>$count</code>
+					is updated dynamically, this slot is re-rendered accordingly.
+					Proof:
+					<code>$count={$count}</code>.
+				</p>
+			</fl-somecomponent>
+		</main>
+	</div>
+</div>
 
 <style>
-	.info {
-		font-size: small;
-		padding-bottom: 25px;
+	.flex {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		margin-bottom: 25px;
+	}
+	.mfeCardBody {
+		padding: 10px;
 	}
 </style>
--- src/lib/SomeComponent.svelte
+++ src/lib/SomeComponent.svelte
@@ -6,45 +6,52 @@
     export let name = "";
 </script>
 
-<div class="some-component">
-    <p class="t">I'm a Svelte Component named {name}. You clicked {$count} time(s).</p>
-
-    <slot>
-        <p>This shows how slots are rendered when no content is provided.</p>
-    </slot>
+<div class="mfeCard">
+    <div class="mfeCardTitle">Component</div>
+    <div class="mfeCardBody">
+        <div class="some-component">
+            <p class="t">
+                I'm a component named {name}. <br /> <br /> The current value of
+                Totoro's store $count is {$count}. Click me to increase the value.
+            </p>
+            <slot>
+                <p>
+                    This shows how slots are rendered when no content is
+                    provided.
+                </p>
+            </slot>
+        </div>
+    </div>
 </div>
 
 <style>
-    .some-component {
-        background: #b7a3d1;
-        padding: 25px;
-        margin-bottom: 25px;
+    .mfeCard {
+        font-size: small;
+        border: 1px solid #b7a3d1;
         border-radius: 5px;
+        overflow: hidden;
+        margin-bottom: 25px;
+    }
+
+    .mfeCardTitle {
+        background: #b7a3d1;
+        color: #333;
+        display: block;
+        border-top-left-radius: 4px;
+        border-top-right-radius: 4px;
+        padding: 3px 6px;
+    }
+
+    .some-component {
+        background: #ccbbe2;
+        padding: 25px;
         cursor: pointer;
         position: relative;
         border: 5px dashed transparent;
     }
 
-    .some-component:hover {
-        border: 5px dashed transparent;
-    }
-
-    .some-component::before {
-        content: "component";
-        display: inline-block;
-        transform: translate(-12.5px, -52px);
-        top: 0;
-        left: 0;
-        font-size: x-small;
-        background-color: #edebfc;
-        color: #333;
-        border: 1px solid #333;
-        padding: 3px 6px;
-        border-radius: 5px;
-    }
-
     p {
-        background: #ab97c5;
+        background: #b7a3d1;
         padding: 25px;
         border-radius: 5px;
         margin-top: 0;
