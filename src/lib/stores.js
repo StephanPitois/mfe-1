4fc8c4da54bd2180cc9ece992e7fb3e8014f0739
Stephan Pitois <spitois@gmail.com>
2023-02-19 02:01:35 -0500

    Show fetch error

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,6 +1,11 @@
 import { readable } from 'svelte/store';
 
 const products_response = await fetch(process.env.PUBLIC_PRODUCTS_API);
+
+if (!products_response.ok) {
+    throw new Error('Products: ' + products_response.status);
+}
+
 const products_data = await products_response.json();
 
 export const products = readable(products_data);
